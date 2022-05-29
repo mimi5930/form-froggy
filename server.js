b7670d341ee0a282623ddbbd888aeb44951e2fef
mimi5930 <mikej.miller440@gmail.com>
2022-05-28 20:08:17 -0500

    added access rights

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const { sendWithName, sendNameless } = require('./controllers/send-email');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.post('/no-name', async (req, res) => {
