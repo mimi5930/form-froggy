711dfa49e371200725d593c3d5658fa5f76d284e
mimi5930 <mikej.miller440@gmail.com>
2022-05-28 19:22:22 -0500

    added async post routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { sendWithName, sendNameless } = require('./controllers/send-email');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -6,9 +7,26 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post('/', (req, res) => {
-  console.log('post request');
-  res.end();
+app.post('/no-name', async (req, res) => {
+  const { email, text } = req.body;
+
+  try {
+    const response = await sendNameless(email, text);
+    res.json(response);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
+app.post('/with-name', async (req, res) => {
+  const { name, email, text } = req.body;
+
+  try {
+    const response = await sendWithName(name, email, text);
+    res.json(response);
+  } catch (e) {
+    res.status(500).json(e);
+  }
 });
 
 app.listen(PORT, () => console.log('Now listening on port', PORT));
