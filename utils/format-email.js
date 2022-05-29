922b30ad417d777c6f2b44585d1e5d131552ea1b
mimi5930 <mikej.miller440@gmail.com>
2022-05-28 19:21:30 -0500

    email formatting

--- utils/format-email.js
+++ utils/format-email.js
@@ -1,9 +1,22 @@
 // format email
-const testOptions = {
-  from: process.env.USER,
-  to: process.env.ADMIN,
-  subject: 'Form submitted',
-  text: 'text'
+const nameEmail = (name, email, text) => {
+  const emailTemplate = {
+    from: process.env.USER,
+    to: process.env.ADMIN,
+    subject: `form submitted by ${name}`,
+    text: `Name: ${name}\nEmail: ${email}\nText: ${text}`
+  };
+  return emailTemplate;
 };
 
-module.exports = { testOptions };
+const namelessEmail = (email, text) => {
+  const emailTemplate = {
+    from: process.env.USER,
+    to: process.env.ADMIN,
+    subject: 'form submitted',
+    text: `Email: ${email}\nText: ${text}`
+  };
+  return emailTemplate;
+};
+
+module.exports = { nameEmail, namelessEmail };
